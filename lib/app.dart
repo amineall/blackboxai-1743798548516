e66754a70acc713ad828c869e460f91a393cd955
amineall <79364185+amineall@users.noreply.github.com>
2025-04-04 21:29:36 +0100

    Add lib/app.dart 1743798574109

---
+++ lib/app.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/screens/home_screen.dart';
+import 'package:music_player/screens/library_screen.dart';
+import 'package:music_player/screens/search_screen.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const LibraryScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Library',
+          ),
+        ],
+      ),
+    );
+  }
+}
